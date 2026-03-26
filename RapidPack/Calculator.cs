5721b21d91575cd76a28202a5a6437a761991107
Krystian <krystian.block@cyfroweszkoly.pl>
2026-03-26 23:49:48 +0100

    Pierwszy commit z staronowegopliku

---
+++ RapidPack/Calculator.cs
@@ -0,0 +1,13 @@
+﻿namespace RapidPack;
+
+public class KalkulatorPrzesylek
+{
+    private const double MaksymalnaWagaKg = 30;
+    private const double CenaBazowa = 10;
+    private const double CenaZaKilogram = 2;
+    private const double DoplatkaEkspres = 15;
+    private const double DoplatkaOstroznieSzklo = 10;
+    private const double CenaPalety = 100;
+    private const double WspolczynnikGabaryt = 0.50;
+    private const double LimitGabarytu = 150;
+}
